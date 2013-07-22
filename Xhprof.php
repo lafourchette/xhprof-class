f2641068aa8450f3853ef8158c69e53924ec6acd
Laurent <lchenay@lafourchette.com>
2013-07-22 09:53:49 +0200

    Allow configuration in construct

--- Xhprof.php
+++ Xhprof.php
@@ -2,21 +2,52 @@
 
 class Xhprof
 {
+    /**
+     * @var float The microtime at the moment of start
+     */
     protected $time = null;
 
-    protected $source = 'default';
+    /**
+     * @var string The source name that will be use in xhprof report
+     */
+    protected $source = null;
 
-    public function start($autoClose = true, $source = null)
+    /**
+     * @var bool If true, no need to call stop()
+     */
+    protected $useShutDown = null;
+
+    /**
+     * @var string The dir where to put log of old report
+     */
+    protected $logDir = null;
+
+    /**
+     * @var string Base url to generate direct link to xhprof page reprot
+     */
+    protected $baseUrl = null;
+
+    /**
+     * @param bool $useShutDown
+     * @param string $source
+     * @param string $logDir
+     * @param string $baseUrl
+     */
+    public function __construct($useShutDown = true, $source = 'default', $logDir = '.', $baseUrl = 'http://xhprof.local')
+    {
+        $this->useShutDown = $useShutDown;
+        $this->source = $source;
+        $this->logDir = $logDir;
+        $this->baseUrl = $baseUrl;
+    }
+
+    public function start()
     {
         $this->time = microtime(true);
 
         require_once '/usr/share/php/xhprof_lib/utils/xhprof_lib.php';
         require_once '/usr/share/php/xhprof_lib/utils/xhprof_runs.php';
         xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
-
-        if ($source !== null) {
-            $this->source = $source;
-        }
 
         register_shutdown_function(array($this, 'stop'));
     }
@@ -28,8 +59,8 @@ class Xhprof
         $run_id = $xhprof_runs->save_run($xhprof_data, $this->source);
 
         // url to the XHProf UI libraries (change the host name and path)
-        $profiler_url = sprintf('http://xhprof.local/?run=%s&source=%s', $run_id, $this->source);
+        $profiler_url = sprintf('%s?run=%s&source=%s', $this->baseUrl, $run_id, $this->source);
 
-        file_put_contents(__DIR__ . '/app/logs/xhprof.log', (microtime(true) - $this->time) . " " . $_SERVER['REQUEST_URI'] . ' - <a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n", FILE_APPEND);
+        file_put_contents($this->logDir . '/xhprof.log', (microtime(true) - $this->time) . " " . $_SERVER['REQUEST_URI'] . ' - <a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n", FILE_APPEND);
     }
 }
