00e0c674671a1348cfaac6626c2d1bf8b3dbcbbf
Laurent <lchenay@lafourchette.com>
2013-07-22 09:44:11 +0200

    No static. Use instance instead

--- Xhprof.php
+++ Xhprof.php
@@ -2,33 +2,34 @@
 
 class Xhprof
 {
-    static $time = null;
+    protected $time = null;
 
-    static $source = 'default';
+    protected $source = 'default';
 
-    public static function start($autoClose = true, $source = null)
+    public function start($autoClose = true, $source = null)
     {
-        self::$time = microtime(true);
+        $this->time = microtime(true);
+
         require_once '/usr/share/php/xhprof_lib/utils/xhprof_lib.php';
         require_once '/usr/share/php/xhprof_lib/utils/xhprof_runs.php';
         xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 
-	if ($source !== null) {
-            self::$source = $source;
+        if ($source !== null) {
+            $this->source = $source;
         }
 
-        register_shutdown_function(array('XHPROF', 'stop'));
+        register_shutdown_function(array($this, 'stop'));
     }
 
-    public static function stop()
+    public function stop()
     {
         $xhprof_data = xhprof_disable();
         $xhprof_runs = new XHProfRuns_Default();
-        $run_id = $xhprof_runs->save_run($xhprof_data, self::$source);
+        $run_id = $xhprof_runs->save_run($xhprof_data, $this->source);
 
         // url to the XHProf UI libraries (change the host name and path)
-        $profiler_url = sprintf('http://xhprof.local/?run=%s&source=%s', $run_id, $profiler_namespace);
+        $profiler_url = sprintf('http://xhprof.local/?run=%s&source=%s', $run_id, $this->source);
 
-        file_put_contents(__DIR__ . '/app/logs/xhprof.log', (microtime(true) - self::$time) . " " . $_SERVER['REQUEST_URI'] . ' - <a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n", FILE_APPEND);
+        file_put_contents(__DIR__ . '/app/logs/xhprof.log', (microtime(true) - $this->time) . " " . $_SERVER['REQUEST_URI'] . ' - <a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n", FILE_APPEND);
     }
 }
